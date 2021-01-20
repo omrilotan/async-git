af4c2aa0bf52e01d866cb8435f942dd404c8eb35
omrilotan <516342+omrilotan@users.noreply.github.com>
2021-01-20 17:53:20 +0200

    Add test for reported vulnerabilities (#13)

---
+++ vulnerabilities/spec.js
@@ -0,0 +1,46 @@
+const { promises: { access, unlink } } = require('fs');
+const wait = require('@lets/wait');
+const git = require('..');
+
+/**
+ * Check if file exists
+ * @param {string}
+ * @returns {boolean}
+ */
+const exists = async path => {
+	try {
+		await access(path);
+		return true;
+	} catch {
+		return false;
+	}
+};
+
+/**
+ * Fail silently and asynchronously
+ * @param {function}
+ * @param {...any}
+ * @returns {any}
+ */
+async function softly(fn, ...args) {
+	try {
+		return await fn(...args);
+	} catch (error) {
+		// ignore
+	}
+}
+
+describe('vulnerabilities', async() => {
+	afterEach(async() => {
+		await wait(100);
+		await softly(unlink, 'HACKED');
+	});
+	it('shell injection in reset', async() => {
+		await softly(git.reset, '; touch HACKED #');
+		expect(await exists('HACKED')).to.be.false;
+	});
+	it('shell injection in tag', async() => {
+		await softly(git.tag, '; touch HACKED #');
+		expect(await exists('HACKED')).to.be.false;
+	});
+});
