e4b7006b86ba420a91aaef628929b9b046949b4e
omrilotan <omri@hamadgera.com>
2021-01-24 20:35:07 +0200

    Use spawn with git to avoid shell script vulnerabilities

--- vulnerabilities/spec.js
+++ vulnerabilities/spec.js
@@ -31,12 +31,20 @@ async function softly(fn, ...args) {
 }
 
 describe('vulnerabilities', async() => {
+	before(async() => {
+		await wait(100);
+		await softly(unlink, 'HACKED');
+	});
 	afterEach(async() => {
 		await wait(100);
 		await softly(unlink, 'HACKED');
 	});
 	it('shell injection in reset', async() => {
 		await softly(git.reset, '; touch HACKED #');
+		expect(await exists('HACKED')).to.be.false;
+	});
+	it('shell injection in reset', async() => {
+		await softly(git.reset, 'a`touch HACKED`b');
 		expect(await exists('HACKED')).to.be.false;
 	});
 	it('shell injection in tag', async() => {
